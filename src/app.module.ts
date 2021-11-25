e817c144fec4b79bf16b75fa4e437644d36433bb
TheDudeAbides1984 <lorenaguey@gmail.com>
2021-11-25 11:20:50 -0500

    ADD: MongoDb integration and User models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/api.keys';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MongooseModule.forRoot(config.mongoURI)],
   controllers: [],
   providers: [],
 })
