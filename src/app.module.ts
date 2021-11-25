5b905e84b49da8083cde6f0b54c2fc7799545d17
TheDudeAbides1984 <lorenaguey@gmail.com>
2021-11-25 16:01:42 -0500

    CONFIG: move Mongo URL to .env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import config from './config/api.keys';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot(config.mongoURI)],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
   controllers: [],
   providers: [],
 })
