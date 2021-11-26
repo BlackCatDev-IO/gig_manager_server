84eb1a4e6dcb0b4d0e98b96bcc51922975b85e1f
TheDudeAbides1984 <lorenaguey@gmail.com>
2021-11-26 10:55:02 -0500

    ADD: Swagger docs & index.html

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT || 4000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Manager')
+    .setDescription('Project Tracker')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     console.log('Server started on port: ' + port);
   });
