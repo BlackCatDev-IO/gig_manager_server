0df9af0cd09ba0ab9dd4fcb09041d1f8b88e34cf
TheDudeAbides1984 <lorenaguey@gmail.com>
2022-03-13 01:06:03 -0500

    CONFIG: update app config files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,22 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from './company/company.module';
 import { JobModule } from './job/job.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      keepAlive: true,
+      connectTimeoutMS: 30000,
+      useUnifiedTopology: true,
+    }),
     UserModule,
     JobModule,
     CompanyModule,
+    InvoiceModule,
+    FileModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
